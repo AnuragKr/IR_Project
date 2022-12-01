1614770afa330fc0435aa8dccb9489f966cbdc8d
AnuragKr <anuragkrsingh02@outlook.com>
2022-12-01 21:57:55 +0530

    Added auto suggestion data api

--- ir_backend/data/urls.py
+++ ir_backend/data/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('insert', views.insert_data),
+    path('insert/autosuggestion', views.insert_auto_suggestion_data),
 ]
--- ir_backend/data/views.py
+++ ir_backend/data/views.py
@@ -12,3 +12,22 @@ def insert_data(request):
     return Response({'No of data inserted': no_data_inserted}, status=status.HTTP_201_CREATED)
   except Exception as err:
     return Response({'Error Message': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(['POST'])
+def insert_data(request):
+  data_service = DataService()
+  try:
+    no_data_inserted = data_service.create_index()
+    return Response({'No of data inserted': no_data_inserted}, status=status.HTTP_201_CREATED)
+  except Exception as err:
+    return Response({'Error Message': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['POST'])
+def insert_auto_suggestion_data(request):
+  data_service = DataService()
+  try:
+    no_data_inserted = data_service.create_auto_index()
+    return Response({'No of data inserted': no_data_inserted}, status=status.HTTP_201_CREATED)
+  except Exception as err:
+    return Response({'Error Message': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
