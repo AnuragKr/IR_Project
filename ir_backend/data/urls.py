8cc08ba6913d1b437108fcf19c8b773c21b5dd17
AnuragKr <anuragkrsingh02@outlook.com>
2022-12-03 23:24:54 +0530

    Added tf-idf data insertion api

--- ir_backend/data/urls.py
+++ ir_backend/data/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('insert', views.insert_data),
+    path('insert/tf_idf', views.insert_tf_idf_data),
     path('insert/autosuggestion', views.insert_auto_suggestion_data),
 ]
--- ir_backend/data/views.py
+++ ir_backend/data/views.py
@@ -31,3 +31,13 @@ def insert_auto_suggestion_data(request):
     return Response({'No of data inserted': no_data_inserted}, status=status.HTTP_201_CREATED)
   except Exception as err:
     return Response({'Error Message': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['POST'])
+def insert_tf_idf_data(request):
+  data_service = DataService()
+  try:
+    no_data_inserted = data_service.create_tf_idf_index()
+    return Response({'No of data inserted': no_data_inserted}, status=status.HTTP_201_CREATED)
+  except Exception as err:
+    return Response({'Error Message': err}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
